be92769249e42a9dc66772dc347a342bfd41e4d2
sohel7 <sohelocp@gmail.com>
2023-08-27 12:14:27 +0600

    Update const_type.dart

--- basic_dart/const_type.dart
+++ basic_dart/const_type.dart
@@ -1,3 +1,6 @@
+// const is a keyword used to declare a compile-time constant value. 
+//  Constants are values that are known at compile time and cannot be changed at runtime.
+
 void main() {
   const int numberOfSchool = 23;
   final String name;
